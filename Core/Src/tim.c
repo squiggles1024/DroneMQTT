0f5776bdbb8a9cd2dfa8331fdd4f158b95c82e8d
Evan Lemson <evanlemson@gmail.com>
2023-02-04 20:14:39 -0800

    Change timer IRQ Frequency

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -107,9 +107,9 @@ void MX_TIM7_Init(void)
 
   /* USER CODE END TIM7_Init 1 */
   htim7.Instance = TIM7;
-  htim7.Init.Prescaler = 59170;
+  htim7.Init.Prescaler = 58053;
   htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim7.Init.Period = 104;
+  htim7.Init.Period = 52;
   htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
   {
